940f48392306d63243987f8f37decdf76166e5b8
Момчил Миланов <deadman_kula@abv.bg>
2021-02-05 15:52:09 +0200

    Add project files.

---
+++ 06. Cake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
