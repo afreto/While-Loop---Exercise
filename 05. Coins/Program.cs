b327dc46a809fa1b33b59a77e0742e5f32d9bfcb
Момчил Миланов <deadman_kula@abv.bg>
2021-02-07 13:38:03 +0200

    solved

--- 05. Coins/Program.cs
+++ 05. Coins/Program.cs
@@ -6,7 +6,65 @@ namespace _05._Coins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double money = double.Parse(Console.ReadLine());
+            double lv = Math.Floor(money);
+            double moneti = Math.Round((money - lv) * 100);
+            int coins = 0;
+            while (lv > 0)
+            {
+                //2
+                if (lv >= 2)
+                {
+                    lv -= 2;
+                    coins++;
+                }
+                //1
+                else if (lv >= 1)
+                {
+                    lv -= 1;
+                    coins++;
+                }
+            }
+            while (moneti > 0)
+            {
+                //0.50
+                if (moneti >= 50)
+                {
+                    moneti -= 50;
+                    coins++;
+                }
+                //0.20
+                else if (moneti >= 20)
+                {
+                    moneti -= 20;
+                    coins++;
+                }
+                //0.10
+                else if (moneti >= 10)
+                {
+                    moneti -= 10;
+                    coins++;
+                }
+                //0.05
+                else if (moneti >= 5)
+                {
+                    moneti -= 5;
+                    coins++;
+                }
+                //0.02
+                else if (moneti >= 2)
+                {
+                    moneti -= 2;
+                    coins++;
+                }
+                //0.01
+                else if (moneti >= 1)
+                {
+                    moneti -= 1;
+                    coins++;
+                }
+            }
+            Console.WriteLine(coins);
         }
     }
 }
--- 06. Cake/Program.cs
+++ 06. Cake/Program.cs
@@ -6,7 +6,22 @@ namespace _06._Cake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int pieces = 0;
+            string takenCake = Console.ReadLine();
+            while (takenCake != "STOP")
+            {
+                int numOfCake = int.Parse(takenCake);
+                pieces += numOfCake;
+                if (pieces >= (width * lenght))
+                {
+                    Console.WriteLine($"No more cake left! You need {pieces - (width * lenght)} pieces more.");
+                    return;
+                }
+                takenCake = Console.ReadLine();
+            }
+            Console.WriteLine($"{(width * lenght) - pieces} pieces are left.");
         }
     }
 }
